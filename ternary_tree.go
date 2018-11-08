d55f08f896e4975d74e2b6d9ea7012c1228e09dc
Alex Tan Hong Pin <alextan220990@gmail.com>
2018-11-08 12:45:02 +0800

    chore(ternary_tree): update logic

--- ternary_tree.go
+++ ternary_tree.go
@@ -1,28 +1,37 @@
 package typeahead
 
-// func main() {
-//         tree := NewTernaryTree()
-//         tree.Add([]rune("hello"))
-//         tree.Add([]rune("ac"))
-//
-//         fmt.Println("Hello, playground", tree.Contains([]rune("hello")))
-//         fmt.Println("Hello, playground", tree.Contains([]rune("xxdda")))
-//
-// }
+package main
 
+import (
+	"fmt"
+)
+
+// REFERENCES:
+// https://www.cs.upc.edu/~ps/downloads/tst/tst.html
 // http://hacktalks.blogspot.com/2012/03/implementing-auto-complete-with-ternary.html
+// https://www.javaworld.com/article/2075027/java-app-dev/plant-your-data-in-a-ternary-search-tree.html?page=2
+// http://www.drdobbs.com/database/ternary-search-trees/184410528?pgno=3
 func main() {
 	tree := NewTernaryTree()
 	tree.Add("hello")
-	tree.Add("helsinki")
 	tree.Add("dobby")
+	tree.Add("debby")
+	tree.Add("dobbs")
+	tree.Add("hells")
+	tree.Add("helrs")
+	tree.Add("helrs")
+	tree.Add("helsinki")
+	tree.Add("hobby")
 	tree.Add("sell")
 	tree.Add("hallo")
+	tree.Add("anglo")
 	tree.Add("hi")
 	tree.Add("car")
-	fmt.Println("has hello:", tree.Contains("hello"))
-	result := tree.NewSearch("h")
+	fmt.Println("has hello:", tree.Contains("dobby"))
+	result := tree.Search("he")
 	fmt.Println(result)
+	fmt.Println(tree.Traverse())
+	tree.NearSearch("dobbs")
 }
 
 type TernaryNode struct {
@@ -70,93 +79,53 @@ func (t *TernaryTree) radd(s []rune, pos int, node *TernaryNode) *TernaryNode {
 }
 
 func (t *TernaryTree) Contains(str string) bool {
-	s := []rune(str)
+	r := []rune(str)
+	result := traverse(t.root, r)
+	if result == nil {
+		return false
+	}
+	return result.endword
+}
+
+// traverse returns the last node that the traversal of the tree returns
+func traverse(node *TernaryNode, r []rune) *TernaryNode {
 	var pos int
-	node := t.root
 	for node != nil {
-		if s[pos] < node.char {
+		switch {
+		case r[pos] < node.char:
 			node = node.left
-		} else if s[pos] > node.char {
+		case r[pos] > node.char:
 			node = node.right
-		} else {
+		default:
 			pos++
-			if pos == len(s) {
-				return node.endword
+			if pos == len(r) {
+				return node
 			}
 			node = node.center
 		}
 	}
-	return false
+	return node
 }
 
 // Search implements an autocomplete for ternary search tree.
-func (t *TernaryTree) Search(str string) {
-	t.search(t.root, []rune(str), "")
-}
-
-func (t *TernaryTree) NewSearch(str string) []string {
-	s := []rune(str)
-	var pos int
-	node := t.root
-	for node != nil {
-		if s[pos] < node.char {
-			node = node.left
-		} else if s[pos] > node.char {
-			node = node.right
-		} else {
-			pos++
-			if pos == len(s) {
-				break
-			}
-			node = node.center
-		}
-	}
-	var result []string
-	t.runedfs(node.center, append(s, node.center.char), &result)
-	return result
-}
-
-func (t *TernaryTree) search(node *TernaryNode, str []rune, match string) {
-	if len(str) > 0 {
-		key := str[0]
-		if key < node.char {
-			t.search(node.left, str, match)
-		} else if key > node.char {
-			t.search(node.right, str, match)
-		} else {
-			if len(str) == 1 {
-				if node.endword {
-					// Match!
-					fmt.Println("match", match, string(node.char))
-				}
-				t.dfs(node.center, match+string(node.char)+string(node.center.char))
-				return
-			}
-			t.search(node.center, str[1:], string(match)+string(key))
-		}
-	}
-}
-
-// Depth first search for ternary tree.
-func (t *TernaryTree) dfs(node *TernaryNode, match string) {
-	if node.endword {
-		fmt.Println("match", match)
-	}
-	if node.center == nil && node.left == nil && node.right == nil {
+func (t *TernaryTree) Search(str string) (result []string) {
+	r := []rune(str)
+	node := traverse(t.root, r)
+	if node == nil {
 		return
 	}
-	if node.center != nil {
-		t.dfs(node.center, match+string(node.center.char))
-	}
-	if node.right != nil {
-		t.dfs(node.right, match[:len(match)-1]+string(node.right.char))
-	}
-	if node.left != nil {
-		t.dfs(node.left, match[:len(match)-1]+string(node.left.char))
-	}
+	t.dfs(node.center, append(r, node.center.char), &result)
+	return
 }
 
-func (t *TernaryTree) runedfs(node *TernaryNode, match []rune, result *[]string) {
+func (t *TernaryTree) Traverse() (result []string) {
+	match := []rune{t.root.char}
+	t.dfs(t.root, match, &result)
+	return
+}
+
+// dfs performs depth-first-search for ternary tree.
+func (t *TernaryTree) dfs(node *TernaryNode, match []rune, result *[]string) {
 	if node.endword {
 		*result = append(*result, string(match))
 	}
@@ -164,14 +133,16 @@ func (t *TernaryTree) runedfs(node *TernaryNode, match []rune, result *[]string)
 		return
 	}
 	if node.center != nil {
-		t.runedfs(node.center, append(match, node.center.char), result)
+		t.dfs(node.center, append(match, node.center.char), result)
 	}
 	if node.right != nil {
 		match[len(match)-1] = node.right.char
-		t.runedfs(node.right, match, result)
+		t.dfs(node.right, match, result)
 	}
 	if node.left != nil {
 		match[len(match)-1] = node.left.char
-		t.runedfs(node.left, match, result)
+		t.dfs(node.left, match, result)
 	}
 }
+
+// TODO: Implement nearest neighbour with hamming distance.
